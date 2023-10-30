8777b53d22354d94cf319b85eee6044e4afe5ff3
licfl <474960201@qq.com>
2023-10-31 00:56:30 +0800

    prism高亮初步开发

--- src/example/atemplate/code.js
+++
@@ -1,6 +0,0 @@
-export default function getCode() {
-  let text = `
-  <p>这是代码片段展示部分.</p>
-  `
-  document.querySelector('#app').insertAdjacentHTML("beforeend", text)
-}
--- src/example/atemplate/description.js
+++
@@ -1,6 +0,0 @@
-export default function getDescription() {
-  let text = `
-  <p>A simple map with an OSM source.</p>
-  `
-  document.querySelector('#app').insertAdjacentHTML("beforeend", text)
-}
