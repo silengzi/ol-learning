efe35d05ace425715bbc742fed070cd87c77fe8d
licfl <474960201@qq.com>
2023-11-02 00:42:18 +0800

    解决无法重新执行已经加载过的js文件的问题

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 import "ol/ol.css"
 
-const fileName = "src/example/Simple Map"
-const script = document.createElement('script');
-script.type = "module"
-script.src = `/${fileName}/index.js`; // 假设你的文件都在同一个目录下
-document.body.appendChild(script);
+// 展示示例详情页
+const showExampleDetails = (filename) => {
+  const filePath = "src/example/" + filename
+  const script = document.createElement('script');
+  script.type = "module"
+  script.src = `/${filePath}/index.js`; // 假设你的文件都在同一个目录下
+  script.src += '?' + new Date().getTime();
+  document.body.appendChild(script);
+}
+
+// 展示示例导航页
+const showExampleList = () => {
+  const filePath = "src/pages/examples"
+  const script = document.createElement('script');
+  script.type = "module"
+  script.src = `/${filePath}.js`; // 假设你的文件都在同一个目录下
+  script.src += '?' + new Date().getTime();
+  document.body.appendChild(script);
+}
+
+// showExampleDetails("Simple Map")
+showExampleList()
---
+++ src/pages/examples.js
@@ -0,0 +1,6 @@
+// 示例导航页面
+document.querySelector('#app').innerHTML = `
+  <h4 id="title">example</h4>
+`
+
+// todo 页面布局
