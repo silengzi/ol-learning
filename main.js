f48b8b23a375784413e78af9b460e5a7726f0193
licfl <474960201@qq.com>
2023-11-05 02:31:11 +0800

    添加示例列表页面和导航栏

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
 import "ol/ol.css"
-
-// 展示示例详情页
-const showExampleDetails = (filename) => {
-  const filePath = "src/example/" + filename
-  const script = document.createElement('script');
-  script.type = "module"
-  script.src = `/${filePath}/index.js`; // 假设你的文件都在同一个目录下
-  script.src += '?' + new Date().getTime();
-  document.body.appendChild(script);
-}
-
-// 展示示例导航页
-const showExampleList = () => {
-  const filePath = "src/pages/examples"
-  const script = document.createElement('script');
-  script.type = "module"
-  script.src = `/${filePath}.js`; // 假设你的文件都在同一个目录下
-  script.src += '?' + new Date().getTime();
-  document.body.appendChild(script);
-}
+import { showExampleList } from "./src/utils/router.js"
 
 // showExampleDetails("Simple Map")
 showExampleList()
+
+document.getElementById("examples-page").addEventListener("click", () => {
+  showExampleList()
+})
