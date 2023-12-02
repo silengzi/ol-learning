09df233710ae6499ffb4fd41e05e78a11e41e22c
silengzi <474960201@qq.com>
2023-12-03 02:39:31 +0800

    初始化

--- main.js
+++
@@ -1,9 +0,0 @@
-// import "ol/ol.css"
-import { showExampleList } from "./src/utils/router.js"
-
-// showExampleDetails("Simple Map")
-showExampleList()
-
-document.getElementById("examples-page").addEventListener("click", () => {
-  showExampleList()
-})
--- src/example/Simple Map/description.js
+++
@@ -1,9 +0,0 @@
-export default function getDescription() {
-  let text = `
-  <p>A simple map with an OSM source.</p>
-  `
-  let text_ch = `
-  <p>一个使用OSM源的简单地图。</p>
-  `
-  document.querySelector('#app').insertAdjacentHTML("beforeend", text_ch)
-}
